87d213de70e6cee33214823916810830fd83752b
yn <Moreseus@outlook.com>
2020-08-03 04:11:53 +0700

    Updates

---
+++ precache-manifest.e5dcd6fa028f484e587ca8785c777c68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c0232d5e3b4b1c4b0773a851ef5131",
+    "url": "/YN-RandomNumber/index.html"
+  },
+  {
+    "revision": "0b917a4d0238e8529ad3",
+    "url": "/YN-RandomNumber/static/css/main.ebf87862.chunk.css"
+  },
+  {
+    "revision": "bdbd1bb650d02c3ce582",
+    "url": "/YN-RandomNumber/static/js/2.f2d533f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/YN-RandomNumber/static/js/2.f2d533f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b917a4d0238e8529ad3",
+    "url": "/YN-RandomNumber/static/js/main.ada98ccd.chunk.js"
+  },
+  {
+    "revision": "db11ebd07271c878458b",
+    "url": "/YN-RandomNumber/static/js/runtime-main.8895ac71.js"
+  },
+  {
+    "revision": "19ad278675393aba977501c41d770d50",
+    "url": "/YN-RandomNumber/static/media/math.19ad2786.png"
+  }
+]);
